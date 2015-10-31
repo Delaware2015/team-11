8e9bad4ef39ba75ac471aa886de65db2bafcd6ae
UCUDProgram <Thegameconn@aol.com>
2015-10-30 22:50:29 -0400

    Create validPassword.java

---
+++ validPassword.java
@@ -0,0 +1,10 @@
+// This function checks to see if the account login password is the same
+
+public class validPassword{
+  public Static int main(String[] args){
+    if(args[0].equals(args[1]))
+        return 0;
+    else
+      return 1
+  }
+}
