34dce4f4e9c497d96eba9034d511b715b1487da5
UCUDProgram <Thegameconn@aol.com>
2015-10-31 11:15:39 -0400

    Update donation_History.java

--- donation_History.java
+++ donation_History.java
@@ -5,19 +5,23 @@ import java .io.IOException;
 import java.util.Arrays;
 
 public class donation_History{
-
+// Variables related to getting the desired data and parsing a file
 public static double yearTotal = 0;
 public static double historyTotal = 0;
 public static String file;
 public static String Year;
 
+/*
+* Takes in a string representing the Donor's account
+* Takes a string representing the csv file Name
+* Takes a string representing the Year that you want the donation records from
+*/
 public static void parseFile(String csvFile, String Year){
 	BufferedReader br = null;
 	String line = "";
 	String csvSplitBy = ",";
-
+// Parses the csv file to the the data the programmer is asking for.
 	try{
-	
 	br = new BufferedReader(new FileReader(csvFile));
 	while( (line = br.readLine()) != null){
 
@@ -44,11 +48,12 @@ public static void parseFile(String csvFile, String Year){
 	}
 }
 
-	public static void main(String[] args){
-/*
-	The first parameter is the csv file, that has the donation history
+// The main Function of the File
+/*	The first parameter is the csv file, that has the donation history
 	The second parameter is the year to compare
 */
+	public static void main(String[] args){
+
 	file = args[0];
 	Year = args[1];
 	parseFile(file,Year);
